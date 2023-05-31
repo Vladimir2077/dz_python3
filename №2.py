b6190af94af39a0959b8e4e3a5f270320c0e1dc9
Vladimir2077 <karlbernadot77@yandex.ru>
2023-05-31 11:38:53 +0200

    Added homework

---
+++ №2.py
@@ -0,0 +1,15 @@
+N = abs(int(input("Введите количество элементов в списке А: ")))
+A = input("Введите через пробел элементы списка: ").split()
+A1 = list(map(int, A))
+if len(A1) != N or N == 0:
+    print("Некорректное количество элементов")
+else:
+    X = int(input("Введите число X, для сравнения со списком: "))
+    max = abs(X + A1[0])
+    index = 0
+    for i in range(1, N):
+        count = abs(X + A1[i])
+        if count < max:
+            max = count
+            index = i
+    print(f"Число {A1[index]} в списке A наиболее близко по величине к числу {X}")
